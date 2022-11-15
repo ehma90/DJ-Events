f54416713f858ccda8bd03f58ba0dffb674b18f3
Emmanuel Essien <ehmaessien@gmail.com>
2022-11-15 15:00:46 +0100

    create Module Aliases with jsconfig; Create  showcase Component

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import footerStyle from "../styles/Footer.module.css";
+import footerStyle from "@/styles/Footer.module.css";
 
 export default function Footer() {
   return (
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import headerStyles from '../styles/Header.module.css'
+import headerStyles from '@/styles/Header.module.css'
 
 export default function Header() {
   return (
